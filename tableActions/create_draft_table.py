b67a47923cbb08a524aea7a5242a6bb2a3f3f919
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-11-27 19:48:16 -0600

    added something to create a draft table

---
+++ tableActions/create_draft_table.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from cfg import engineStr
+from sqlalchemy.orm import sessionmaker
+from Tables.Draft import Base, Draft
+
+def create_draft_table():
+    engine = create_engine(engineStr, echo=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+
+    try:
+        session = session()
+        session.commit()
+        print("Draft table created!")
+
+    except Exception as e:
+        session.rollback()
+        print(f"An error occurred: {e}")
+
+    finally:
+        session.close()
+
