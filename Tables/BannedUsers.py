337b8af3beb0b6176b62012eb0eaad2d9a2c29d1
nameofme01 <69026352+nameofme01@users.noreply.github.com>
2024-11-27 19:33:59 -0600

    added a banned users table

---
+++ Tables/BannedUsers.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+class BannedUsers(Base):
+    __tablename__ = 'banned_users'
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
---
+++ tableActions/create_banned_users_table.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from cfg import engineStr
+from sqlalchemy.orm import sessionmaker
+from Tables.BannedUsers import Base, BannedUsers
+
+def create_banned_users_table():
+    engine = create_engine(engineStr, echo=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+
+    try:
+        session = session()
+        session.commit()
+        print("Banned Users table created!")
+
+    except Exception as e:
+        session.rollback()
+        print("An error occurred: {e}")
+
+    finally:
+        session.close()
