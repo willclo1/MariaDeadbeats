e0c682d1a4a06d0b2596f50798107c6f09ca4438
willclo1 <115596953+willclo1@users.noreply.github.com>
2024-12-08 14:36:25 -0600

    fixed an error with the user logs table

--- tableActions/csi3335f2024.py
+++ tableActions/csi3335f2024.py
@@ -1,5 +1,5 @@
 # Modify for your own database connection/account
-cfg = {"host" : "localhost", "user": "nicholasnolen", "password" : "", "db" : "MariaDeadbeats"}
+cfg = {"host" : "localhost", "user": "willclore", "password" : "password", "db" : "MariaDeadbeats"}
 engineStr = "mysql+pymysql://" + cfg.get("user") + ":" + cfg.get("password") + "@" + cfg.get(
         "host") + ":3306/" + cfg.get("db")
 API_KEY = "AIzaSyD_0DLUddBAgE3IuRWlp7HBQgTu0qq2Kik"
